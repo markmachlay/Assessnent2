ba531576d3e7d42fd396b9bfa04e9a6eeb079944
markmachlay <84604813+markmachlay@users.noreply.github.com>
2021-06-28 18:00:40 -0400

    Add files via upload

---
+++ MockAssessment2/Challenge.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace MockAssessment2
+{
+    public class Challenge
+    {
+        public int Result { get; set; }
+        public string Max { get; set; }
+        public double DroidAverage { get; set; }
+        public List<string> ListOfPlanets { get; set; }
+        public Challenge()
+        {
+        }
+        public int AddStarWarsCharacters(string[] starWarsCharacters)
+        {
+            Result = 0;
+            for (int i = 0; i < starWarsCharacters.Length; i++)
+            {
+                if(starWarsCharacters[i] == "Yoda")
+                {
+                    Result = i;
+                }
+            }
+            return Result;
+        }
+
+        public string DeathStarCombat(Dictionary<string,int> starWarsPowerLevels)
+        {
+           Max = starWarsPowerLevels.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+           return Max;
+        }
+
+        public List<string> ConvertPlanets(string[] Planets)
+        {
+            ListOfPlanets = Planets.Reverse().ToList();
+            return ListOfPlanets;
+        }
+
+        public double AverageDroids(List<int> Droids)
+        {
+            var doubleDroids = Droids.Select(i => (double)i).ToList();
+            var evenDroids = new List<int>();
+            foreach (var droid in Droids)
+            {
+                if (droid % 2 == 0)
+                {
+                    evenDroids.Add(droid);
+                }
+            }
+            double total = evenDroids.Sum();
+            DroidAverage = total / evenDroids.Count;
+            return DroidAverage;
+        }
+
+        public string TryToCatchDarthVader(string Name)
+        {
+            try
+            {
+                int result = int.Parse(Name);
+                Console.WriteLine("Vader Was Captured!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Vader Got Away!");
+            }
+            return null;
+        }
+    }
+}
---
+++ MockAssessment2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MockAssessment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var starWarsArray = new string[] { "Yoda", "Leia", "Luke", "Rey" };
+            var challenge = new Challenge();
+            var result = challenge.AddStarWarsCharacters(starWarsArray);
+            Console.WriteLine(result);
+
+            var starWarsPowerLevels = new Dictionary<string, int>();
+            starWarsPowerLevels.Add("Luke Skywalker", 910);
+            starWarsPowerLevels.Add("Darth Vader", 225);
+            starWarsPowerLevels.Add("Obi-wan Kenobi", 120);
+            starWarsPowerLevels.Add("Yoda", 100);
+            var challenge2 = new Challenge();
+            var max = challenge2.DeathStarCombat(starWarsPowerLevels);
+            Console.WriteLine(max);
+
+            var planets = new string[] { "Dagobah", "Yavin-4", "Tatooine", "Datooine" };
+            var challenge3 = new Challenge();
+            var reversePlanets = challenge3.ConvertPlanets(planets);
+            foreach (var planet in reversePlanets)
+            {
+                Console.WriteLine(planet);
+            }
+
+            List<int> listOfDroids = new List<int>();
+            listOfDroids.Add(1000);
+            listOfDroids.Add(2000);
+            listOfDroids.Add(4000);
+            listOfDroids.Add(3000);
+            var challenge4 = new Challenge();
+            var evenDroids = challenge4.AverageDroids(listOfDroids);
+            Console.WriteLine(evenDroids);
+
+
+            var challenge5 = new Challenge();
+            var isDarthVader = challenge5.TryToCatchDarthVader("Nope");
+            var isDarthVaderAgain = challenge5.TryToCatchDarthVader(null);
+        }
+    }
+}
